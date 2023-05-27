42cc5a31b7f6ccdbf99f8faa636626d2ea078031
Swain Molster <swain.molster@lifeomic.com>
2023-05-27 18:53:49 -0400

    fix build

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const { unlinkSync } = require('fs');
+const { unlinkSync, readdirSync } = require('fs');
 const glob = require('glob');
 
 const run = (cmd) => execSync(cmd, { cwd: __dirname, stdio: 'inherit' });
@@ -7,6 +7,11 @@ const run = (cmd) => execSync(cmd, { cwd: __dirname, stdio: 'inherit' });
 run('rm -rf dist/');
 
 run('yarn tsc');
+
+for (const file of readdirSync('dist/src')) {
+  run(`mv dist/src/${file} dist/${file}`);
+}
+run('rm -rf dist/src dist/test');
 
 for (const file of ['package.json', 'README.md']) {
   run(`cp ${file} dist/`);
